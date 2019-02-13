2a255a52c05d6151725cdc8bde46849d723a5202
Kevin Sanchez <kevin.o.sanchez@gmail.com>
2019-02-13 15:49:23 -0800

    Update ToDos.vue

--- client/src/views/ToDos.vue
+++ client/src/views/ToDos.vue
@@ -1,54 +1,124 @@
 <template>
-    <div class="todos">
+    <div class='todos'>
         <table>
             <thead>
-                <th></th>
+                <th/>
                 <th> Item </th>
                 <th> Due Date </th>
+                <th/>
             </thead>
         <!--actual to do list -->
         <!--group the box with todo item-->
             <tbody>
                 <tr v-for="(todo, index) in mytodos" v-bind:key="index">
-                    <td><input type="checkbox"></td>
-                    <td>{{ todo.name }}</td>
-                    <td>{{ "\t" + todo.duedate + "\t"}}</td>
+                    <td><input type="checkbox" v-on:change="markDone(index)" v-bind:checked="todo.complete"></td>
+                    <td>{{ todo.title }}</td>
+                    <td>{{ todo.dueDate || "N/A" }}</td>
                     <td><button class="button" v-on:click="deleteTodoItem(todo.id)"> Delete </button></td>
                 </tr>
             </tbody>
         </table>
-        <div>
-            <span> Title <br><input v-model="message" size= "30px" type="text" ></span>
-            <br>
-            <span> Due Date <br><input  v-model="due_date" class="due-date" size="40px" type="date" ></span>
+        <div class="add-new">
+            <button class="button" v-on:click="addTodoItem"> Add </button>
+            <input id="dateInput" type="date" class="input"/>
+            <input id="descInput" type="text" placeholder="ToDo description" class="input/">
         </div>
-        <button class="button" v-on:click="addTodoItem(message, due_date)"> Add </button>
+        
     </div>
 </template>
 
 <script lang="ts">
+import axios, { AxiosError, AxiosResponse } from "axios";
 import Vue from 'vue';
+import { APIConfig } from "../utils/api.utils";
 import { Component } from "vue-property-decorator";
 @Component
 export default class ToDos extends Vue {
     idVal: number = 0;
     mytodos: ToDo[] = [
-        { name: "todo one", duedate: undefined, id: this.idVal++},
-        { name: "todo two", duedate: undefined, id: this.idVal++},
-        { name: "todo three", duedate: undefined, id: this.idVal++}
+        /*{ name: "Todo one", duedate: undefined},
+        { name: "todo two", duedate: undefined, done: true },
+        { name: "todo three", duedate: undefined}*/
+        { title: "todo one", dueDate: undefined},
+        { title: "todo two", dueDate: undefined, complete: true },
+        { title: "todo three", dueDate: undefined}
+        
     ];
-    addTodoItem(message: string, due_date: Date) {
-        this.mytodos.push({name: message, duedate: due_date, id: this.idVal++});
+    addTodoItem() {
+        const dueDate = (<HTMLInputElement>document.getElementById("dateInput")).value;
+        const description = (<HTMLInputElement>document.getElementById("descInput")).value;
+        if(description) {
+            const todo: ToDo = {title: description, dueDate: dueDate ? new Date(dueDate) : undefined, complete: false};
+            axios
+                .post(APIConfig.buildUrl("/todos"), todo, { 
+                    headers: {
+                        token: this.$store.state.userToken
+                    }
+                })
+                .then((response) => {
+                    this.mytodos.push(response.data.todo);
+                });
+        }
     }
-    deleteTodoItem(id: number) {
-        const index: number = this.mytodos.findIndex((todo: ToDo) => todo.id == id);
-        this.mytodos.splice(index, 1);
+    deleteTodoItem(index: number) {
+        const todo = this.mytodos[index];
+        axios
+            .delete(APIConfig.buildUrl("/todos/" + todo.id), {
+                headers: {
+                    token: this.$store.state.userToken
+                }
+            })
+            .then(() => {
+                this.mytodos.splice(index, 1);
+            });
+    }
+    markDone(index: number) {
+        const complete = !this.mytodos[index].complete;
+        this.mytodos[index].complete = complete;
+        const todo = this.mytodos[index];
+        axios
+            .put(APIConfig.buildUrl("/todos/" + todo.id), { complete: complete }, {
+                headers: {
+                    token: this.$store.state.userToken
+                }
+            })
+    }
+    loadTodos() {
+        if (this.$store.state.userToken) {
+            axios
+                .get(APIConfig.buildUrl("/todos/load"), {
+                    headers: {
+                        token: this.$store.state.userToken
+                    }
+                })
+                .then((response) => {
+                    this.mytodos = response.data;
+                });
+        }
+    }
+    mounted() {
+        this.loadTodos();
+
+        this.$store.watch(
+            (state) => state.userToken,
+            (newValue, oldValue) => {
+                if (newValue) {
+                    this.loadTodos();
+                }
+            }
+        );
+    }
+    beforeRouteUpdate(to: string, from: string, next: string) {
+        if (to == "todos") {
+            this.loadTodos();
+        }
     }
 }
 interface ToDo {
-    name: string;
-    duedate: Date | undefined;
-    id: number;
+    title: string;
+    dueDate?: Date;
+    complete?: boolean;
+    id?: number;
 }
 </script>
 
